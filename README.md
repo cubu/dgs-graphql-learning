8b1d7694721ecd94a2631fe2e489822719fc7b9a
haozhiqiang <haozhiqiangdade@163.com>
2021-03-11 16:23:11 +0800

    解决了游标数组越界的问题

--- README.md
+++ README.md
@@ -1,7 +1,153 @@
-# dgs-graphql-learning
-source: https://netflix.github.io/dgs/
+GraphQL Spring Boot 框架：[DGS ](https://netflix.github.io/dgs/)
 
-springboot 指定配置文件
-配置文件命名： application-{env}, {}内的env根据环境进行命名
-系统参数：-Dspring.profiles.active=dev （启动 application-dev 的配置）
-端口
+## Quick Start
+
+### Step 1 项目初始化
+
+
+
+
+
+### Step 2
+
+### Step3
+
+### Step4
+
+1、业务模拟
+
+**实体**：小组（Team）， 成员（User）
+
+```
+Team: 
+    id : String          // id
+	name : String        // 组名
+	description : String // 描述
+	users : List<User>   // 组员列表
+```
+
+```
+User: 
+    teamId : String      //所属小组id
+	name : String        // 姓名
+```
+
+**实现功能**
+
+查询 Team；
+
+Team 中的 Users 支持分页。
+
+查询 Teams（Team列表），支持分页;
+
+User不可脱离Team查询
+
+
+
+**2、build.gradle**
+
+`DGS` 依赖需要从 `jcenter` 仓库获取
+
+```groovy
+plugins {
+	id 'org.springframework.boot' version '2.4.3'
+	id 'io.spring.dependency-management' version '1.0.11.RELEASE'
+	id "com.netflix.dgs.codegen" version "4.0.10"
+	id 'application'
+	id 'java'
+}
+
+group = 'com.**'
+version = '0.0.1-SNAPSHOT'
+sourceCompatibility = '1.8'
+
+configurations {
+	compileOnly {
+		extendsFrom annotationProcessor
+	}
+}
+
+repositories {
+	jcenter()
+}
+
+dependencies {
+	implementation "com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter:latest.release"
+	implementation 'org.springframework.boot:spring-boot-starter-web'
+	implementation("com.github.javafaker:javafaker:1.+")
+	compileOnly 'org.projectlombok:lombok'
+	annotationProcessor 'org.projectlombok:lombok'
+	testImplementation 'org.springframework.boot:spring-boot-starter-test'
+}
+
+generateJava{
+	schemaPaths = ["${projectDir}/src/main/resources/schema"] // List of directories containing schema files
+	packageName = 'com.**.**' // The package name to use to generate sources
+	generateClient = true // Enable generating the type safe query API
+}
+
+test {
+	useJUnitPlatform()
+}
+
+```
+
+
+
+3、定义Schema
+
+schema路径：`**/src/main/resources/schema/schema.graphqls`。
+
+
+
+提供了两个查询方法：
+
+- team：输入`teamId`获取`team`
+- teams：输入分页信息，获取 `List<Team>`
+
+```
+schema {
+    query: Query
+}
+
+type Query {
+  team(id: String): Team
+  teams(first: Int, after: String): [Team]
+  teams()
+}
+
+type Team {
+    id : ID
+	name : String
+	description : String
+	users(first: Int, after: String) : [User]
+}
+
+type User {
+    teamId: String
+	name : String    
+}
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/wuwei/dgs/datafetchers/PageMoviesDataFetcher.java
+++ src/main/java/com/wuwei/dgs/datafetchers/PageMoviesDataFetcher.java
@@ -9,7 +9,6 @@ import java.lang.String;
 import com.wuwei.dgs.constants.DgsConstants;
 import com.wuwei.dgs.service.MovieService;
 import com.wuwei.dgs.types.*;
-import graphql.schema.DataFetchingEnvironment;
 import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.StandardCharsets;
@@ -32,50 +31,54 @@ public class PageMoviesDataFetcher {
     @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.PageMoviesWithCursor)
     public MoviesConnection pageMovies(@InputArgument("first") Integer first,
                                        @InputArgument("after") String after) {
-
+        int size = edges.size();
         if (StringUtils.isEmpty(after)) {
-          return getConnection(edges.subList(0, first), first);
+            first = Math.min(first, size);
+            return getConnection(edges.subList(0, first), false, first < edges.size());
         }
 
         boolean found = false;
-        int index = 0;
+        int index = -1;
         List<MovieEdge> list = new ArrayList<>();
 
-        for(int i = 0; i < edges.size(); i++) {
-          if (!found && !edges.get(i).getCursor().equals(after)) {
-            continue;
-          }
-          if (!found) {
-            found = true;
-            index = i;
-            continue;
-          }
-          list.add(edges.get(i));
+        for (int i = 0; i < size; i++) {
+            if (!found && !edges.get(i).getCursor().equals(after)) {
+                continue;
+            }
+            if (!found) {
+                found = true;
+                index = i;
+                continue;
+            }
+            if (list.size() == first) {
+                break;
+            }
+            list.add(edges.get(i));
         }
-        return getConnection(list, index+1);
+        return getConnection(list, index != -1, index + first + 1 < size);
     }
 
-    private MoviesConnection getConnection(List<MovieEdge> edges, int first) {
-      return MoviesConnection.newBuilder()
-              .totalCount(edges.size())
-              .edges(edges)
-              .pageInfo(
-                      PageInfo.newBuilder()
-                              .hasPreviousPage(false)
-                              .hasNextPage(first < edges.size())
-                              .startCursor(edges.get(0).getCursor())
-                              .endCursor(edges.get(edges.size() - 1).getCursor())
-                              .build()
-              )
-              .build();
+    private MoviesConnection getConnection(List<MovieEdge> list, boolean hasPrevious, boolean hasNext) {
+        return MoviesConnection.newBuilder()
+                .totalCount(edges.size())
+                .edges(list)
+                .pageInfo(
+                        PageInfo.newBuilder()
+                                .hasPreviousPage(hasPrevious)
+                                .hasNextPage(hasNext)
+                                .startCursor(edges.get(0).getCursor())
+                                .endCursor(edges.get(edges.size() - 1).getCursor())
+                                .build()
+                )
+                .build();
     }
 
     @DgsData(parentType = DgsConstants.QUERY_TYPE, field = DgsConstants.QUERY.PageMovies)
     public PageList pageMovies(@InputArgument("pageSize") Integer pageSize,
                                @InputArgument("pageNum") Integer pageNum) {
 
-        int start = (pageNum-1) * pageSize;
-        int end = start + pageSize;
+        int start = (pageNum - 1) * pageSize;
+        int end = Math.min(start + pageSize, MovieService.movieList().size());
         List<Movie> movies = MovieService.movieList().subList(start, end);
         List<Node> nodes = new ArrayList<>(movies);
 
